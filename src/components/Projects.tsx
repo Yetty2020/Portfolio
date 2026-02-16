f4d2cd68d0e18079e34910a1876679143ed8ffb3
Yetty2020 <fatihaadetoro@gmail.com>
2026-02-16 22:06:37 +0100

    Stackcards height fix

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -17,8 +17,8 @@ export default function Projects() {
 
 
   return (
-    <main className='bg-black  mb-10 '>
-       <h1 className='text-white text-3xl mb-5 '>Selected Projects</h1>
+    <main className='bg-black  md:mb-10 '>
+       <h1 className='text-white text-3xl mb-20 md:mb-5 '>Selected Projects</h1>
        <StackedCards/>
       
 
